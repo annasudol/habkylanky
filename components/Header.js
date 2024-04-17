0817d8781910eeeaf3772cac69e0f666117dc407
Anna Sudol <sudolann@gmail.com>
2024-04-17 11:29:49 +0200

    changes

--- components/Header.js
+++ components/Header.js
@@ -7,13 +7,14 @@ const Header = () => {
     <header className="container flex text-center mx-auto pb-6">
       <div className="mx-auto">
         <Image
-          height={96}
-          width={96}
+          height={106}
+          width={106}
           src="/profile-pic.png"
           alt="Hanky Lanky"
-          className="h-24 rounded-full mx-auto mb-5 shadow-lg"
+           style="vertical-align:middle"
+          className="h-24 w-24 rounded-full mx-auto  flex justify-center items-center "
         />
-        <h1 className="font-bold text-2xl mb-1 text-[#f1efe7]">{info.name}</h1>
+        <h1 className="font-bold text-2xl mb-1 text-[#f1efe7] mt-4">{info.name}</h1>
         <p className="whitespace-break-spaces mb-4 tracking-wide text-[#f1efe7]">
           {info.title}
         </p>
--- components/SocialLinks.js
+++ components/SocialLinks.js
@@ -4,7 +4,61 @@ import Image from "next/image";
 function SocialLinks() {
   return (
     <ul className="flex gap-5 justify-center">
-      {socialLinks.map((link) => (
+         <li className="h-8 flex items-center">
+          <a
+            className="text-[#f1efe7] hover:scale-125 transition-colors duration-200"
+            href="mailto:hankylankyy@gmail.com"
+            target="_blank"
+            rel="noopener"
+            alt="Hanky Lanky"
+          >
+
+             <Image
+          height={34}
+          width={34}
+          src={`/mail.svg`}
+          alt="Hanky Lanky email"
+          
+        />
+          </a>
+        </li>
+      <li className="h-8 flex items-center">
+          <a
+            className="text-[#f1efe7] hover:scale-125 transition-colors duration-200"
+            href="https://www.instagram.com/hanky__lanky"
+            target="_blank"
+            rel="noopener"
+            alt="Hanky Lanky instagram"
+          >
+
+             <Image
+          height={32}
+          width={32}
+          src={`/instagram.svg`}
+          alt="Hanky Lanky"
+          
+        />
+          </a>
+        </li>
+        <li className="h-8 flex items-center">
+          <a
+            className="text-[#f1efe7] hover:scale-125 transition-colors duration-200"
+            href="https://soundcloud.com/hankylanky"
+            target="_blank"
+            rel="noopener"
+            alt="Hanky Lanky soundcloud"
+          >
+
+             <Image
+          height={50}
+          width={50}
+          src={`/soundcloud.svg`}
+          alt="Hanky Lanky soundcloud"
+          
+        />
+          </a>
+        </li>
+      {/* {socialLinks.map((link) => (
         <li className="h-8 flex items-center" key={link.icon}>
           <a
             className="text-[#f1efe7] hover:scale-125 transition-colors duration-200"
@@ -23,7 +77,7 @@ function SocialLinks() {
         />
           </a>
         </li>
-      ))}
+      ))} */}
     </ul>
   );
 }
